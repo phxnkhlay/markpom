d65b9060fef5a28643c4fb3b75c55b0cc0276f7e
phxnkhlay <zeempleunix@gmail.com>
2026-07-02 01:48:46 +0700

    Update update_github_file.py

--- update_github_file.py
+++ update_github_file.py
@@ -318,7 +318,7 @@ def main():
         force_expired = None  # auto per-file
 
     # Proses semua file target
-    target_files = generate_target_files(month_name="JANUARI, year=2027, prefix="PM")
+    target_files = generate_target_files(month_name="JANUARI", year=2027, prefix="MP")
     print(f"\n📁 Daftar file target ({len(target_files)}):")
     print(target_files)
  
